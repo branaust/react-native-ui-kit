74d6c678627bb8f0590538e07ad6d81019291401
Brandon Austin <61210295+branaust@users.noreply.github.com>
2023-11-19 12:43:02 -0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import {
+  NativeModulesProxy,
+  EventEmitter,
+  Subscription,
+} from "expo-modules-core";
 
 // Import the native module. On web, it will be resolved to ReactNativeUiKit.web.ts
 // and on native platforms to ReactNativeUiKit.ts
-import ReactNativeUiKitModule from './ReactNativeUiKitModule';
-import ReactNativeUiKitView from './ReactNativeUiKitView';
-import { ChangeEventPayload, ReactNativeUiKitViewProps } from './ReactNativeUiKit.types';
+import {
+  ChangeEventPayload,
+  ReactNativeUiKitViewProps,
+} from "./ReactNativeUiKit.types";
+import ReactNativeUiKitModule from "./ReactNativeUiKitModule";
+import ReactNativeUiKitView from "./ReactNativeUiKitView";
 
 // Get the native constant value.
 export const PI = ReactNativeUiKitModule.PI;
@@ -17,10 +24,14 @@ export async function setValueAsync(value: string) {
   return await ReactNativeUiKitModule.setValueAsync(value);
 }
 
-const emitter = new EventEmitter(ReactNativeUiKitModule ?? NativeModulesProxy.ReactNativeUiKit);
+const emitter = new EventEmitter(
+  ReactNativeUiKitModule ?? NativeModulesProxy.ReactNativeUiKit
+);
 
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function addChangeListener(
+  listener: (event: ChangeEventPayload) => void
+): Subscription {
+  return emitter.addListener<ChangeEventPayload>("onChange", listener);
 }
 
 export { ReactNativeUiKitView, ReactNativeUiKitViewProps, ChangeEventPayload };
